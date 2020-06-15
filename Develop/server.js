245fa46b2b6c3cb860168bbab3edf9191fb3d515
Samuel Thompson <thomps9012@gmail.com>
2020-06-15 12:16:49 -0400

    added in functionality to create Exercise databases

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 //requiring our express application and linking to our different exercise javascript schema files
 const Resistance = require("./models/resistance.js");
 const Cardio = require("./models/cardio.js");
+const Exercise = require("./models/Exercise.js");
 const app = express();
 
 
@@ -28,41 +29,59 @@ Exercise.create({ name: "Exercise History" })
   .then(dbExercise => {
     console.log(dbExercise);
   })
-  .catch(({message}) => {
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+//creates our resistance database
+Resistance.create({ name: "Resistance Exercise History" })
+  .then(dbExercise => {
+    console.log(dbExercise);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+//creates our cardio database
+Cardio.create({ name: "Cardio Exercise History" })
+  .then(dbExercise => {
+    console.log(dbExercise);
+  })
+  .catch(({ message }) => {
     console.log(message);
   });
 
 //sets up an app.post for a resistance workout
 app.post("/submit", ({ body }, res) => {
-    Resistance.create(body)
-      .then(dbExercise => {
-        res.json(dbExercise);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  Resistance.create(body)
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 //sets up an app.post for a cardio workout
 app.post("/submit", ({ body }, res) => {
-    Cardio.create(body)
-      .then(dbExercise => {
-        res.json(dbExercise);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  Cardio.create(body)
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  //sets up a get request for retrieving all of our cardio data
-
-
-  //sets up a get request for retrieving all of our resistance data
+//sets up a get request for retrieving all of our cardio data
 
 
-  //sets up a get request for retrieving all of our workout data
+//sets up a get request for retrieving all of our resistance data
 
-  
+
+//sets up a get request for retrieving all of our workout data
+
+
 //setting up our port and console logging that the port is running correctly
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
