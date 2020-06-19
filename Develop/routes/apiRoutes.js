ba1dc5431573c7b89399b2ea6c91f60f464b4b3c
Samuel Thompson <thomps9012@gmail.com>
2020-06-19 10:40:44 -0400

    updated api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
+module.exports = function(app){
 //get route for retrieving workout data from the last workout
-router.get("/api/workouts", (req, res) => {
+app.get("/api/workouts", (req, res) => {
     Workout.find()
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+        .then(data => {
+            res.json(data);
         })
         .catch(err => {
             res.json(err);
@@ -13,10 +13,10 @@ router.get("/api/workouts", (req, res) => {
 });
 
 //post route for creating a new resistance or cardio workout
-router.post("/api/workouts", (req, res) =>{
+app.post("/api/workouts", (req, res) =>{
     Workout.create({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+        .then(data => {
+            res.json(data);
         })
         .catch(err => {
             res.json(err);
@@ -24,7 +24,7 @@ router.post("/api/workouts", (req, res) =>{
 });
 
 //put route for updating a workout
-router.put("/api/workouts/:id", ({ body, params}, res => {
+app.put("/api/workouts/:id", ({ body, params}, res => {
     Workout.findByIdAndUpdate(
         params.id,
         {
@@ -32,8 +32,8 @@ router.put("/api/workouts/:id", ({ body, params}, res => {
             //runs a validator to ensure new exercises meet our schema
             { new: true, runValidators: true}
     )
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+    .then(data => {
+        res.json(data);
     })
     .catch(err => {
         res.json(err);
@@ -41,25 +41,14 @@ router.put("/api/workouts/:id", ({ body, params}, res => {
 }));
 
 //get route for retrieving workout data from a specific range of dates
-router.get("/api/workouts/range", ({ query }, res) => {
+app.get("/api/workouts/range", ({ query }, res) => {
     Workout.find({ day: { $gte: query.start, $lte: query.end } })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
+        .then(data => {
+            res.json(data);
         })
         .catch(err => {
             res.json(err);
         });
 });
 
-//delete route for finding a workout by an id number and deleting
-router.delete("/api/workouts", ({ body}, res) => {
-    Workout.findByIdAndDelete(body.id)
-        .then(() => {
-            res.json(true);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-module.exercises = router;
+};
