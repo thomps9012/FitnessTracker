db5c75fc15075ff058e5d3d22f6267d9ee89c616
Samuel Thompson <thomps9012@gmail.com>
2020-06-19 10:07:59 -0400

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -7,10 +7,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 //requiring our express application and linking to our different exercise javascript schema files
-const Resistance = require("./models/resistance.js");
-const Cardio = require("./models/cardio.js");
-const Exercise = require("./models/Exercise.js");
-// const db = require("./models");
+const Workout = require("./models/Workout.js");
 const app = express();
 
 
@@ -25,87 +22,17 @@ app.use(express.static("public"));
 //ensuring that mongoose is connected to our local host
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
-//creates our exercise database
-Exercise.create({ name: "Exercise History" })
-  .then(dbExercise => {
-    console.log(dbExercise);
+//creates our Workout database
+Workout.create({ name: "Workout" })
+  .then(dbWorkout => {
+    console.log(dbWorkout);
   })
   .catch(({ message }) => {
     console.log(message);
   });
-
-//creates our resistance database
-Resistance.create({ name: "Resistance Exercise History" })
-  .then(dbExercise => {
-    console.log(dbExercise);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
-//creates our cardio database
-Cardio.create({ name: "Cardio Exercise History" })
-  .then(dbExercise => {
-    console.log(dbExercise);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
-//sets up an app.post for a resistance workout
-app.post("/submit", ({ body }, res) => {
-  Resistance.create(body)
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//sets up an app.post for a cardio workout
-app.post("/submit", ({ body }, res) => {
-  Cardio.create(body)
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//sets up a get request for retrieving all of our cardio data
-app.get("/Cardio", (req, res) => {
-  Cardio.find({})
-    .then(dbCardio => {
-      res.json(dbCardio);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//sets up a get request for retrieving all of our resistance data
-app.get("/Resistance", (req, res) => {
-  Resistance.find({})
-    .then(dbResistance => {
-      res.json(dbResistance);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//sets up a get request for retrieving all of our workout data
-app.get("/exercise", (req, res) => {
-  Exercise.find({})
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+//routes
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"))
 
 //setting up our port and console logging that the port is running correctly
 app.listen(PORT, () => {
