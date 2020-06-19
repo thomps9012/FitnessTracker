ad14478d9606fd0e2166d1aea3182a77aadafb08
Samuel Thompson <thomps9012@gmail.com>
2020-06-19 09:38:53 -0400

    updated fitness models to just exercises and index

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -4,46 +4,66 @@ const mongoose = require("mongoose");
 //sets up a new mongoose schema
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const WorkoutSchema = new Schema({
     //sets the date of the workout for date now
     day: {
         type: Date,
         default: Date.now
     },
+    exercises: [{
     //sets the type of workout
     type: {
         type: String,
-        trim: true
+        trim: true,
+        required: "Enter an exercise type"
     },
     //sets the name of the workout to a string
     name: {
         type: String,
-        trim: true
+        trim: true,
+        required: "Enter an exercise name"
     },
     //sets the duration of the workout to a number
     duration: {
         type: Number,
-        trim: true
+        required: "Enter an exercise duration in minutes"
     },
     //sets the weight of the workout to a number
     weight: {
-        type: Number,
-        trim: true
+        type: Number
     },
     //sets the reps of the workout to a number
     reps: {
-        type: Number,
-        trim: true
+        type: Number
     },
     //sets the "sets" of a workout to a number
     sets: {
-        type: Number,
-        trim: true
+        type: Number
+    },
+    //sets the distance to a number
+    distance: {
+        type: Number
     }
+}]
+},
+{
+    toJSON: {
+        //includes properties when data is requested
+        virtuals: true
+    }
+}
+);
+
+//adds a dynamically-created property to the schema
+WorkoutSchema.virtual("totalDuration").get(function() {
+    //reduces the array of exercises down to the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 //sets our workoutSchema as a mongoose model
-const workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 //exports our ResistanceSchema to be called in our server.js file
-module.exports = workoutSchema;
+module.exports = Workout;
---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    
+    //get route for retrieving workout data from the last workout
+
+    //post route for creating a new resistance or cardio workout
+
+    //put route for updating a workout
+
+    //get route for retrieving workout data from a specific range of dates
+}
